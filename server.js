a125ef782ce74f68f7ed0c49897972585d18609f
Nikki Petersen <piknik@me.com>
2019-04-23 06:18:43 -0600

    changed port for mongo

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ require("./routes/routes.js")(app);
 
 
 // Listen on port 3000
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("App running on port 3002!");
 });
